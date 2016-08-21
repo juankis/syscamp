8a7011a1b399b39d508bb3abc12e8b9465a06759
juankis <j_carlos1111@hotmail.com>
2016-08-21 16:14:19 -0400

    paginas en construccion

---
+++ resources/views/enConstruccion.blade.php
@@ -0,0 +1,17 @@
+@extends('layouts.admin')
+
+@section('titleContent')
+<h2>En <strong>Construccion</strong></h2>
+@endsection
+@section('titlePanel')
+<h3 class="panel-title"><strong>En </strong>Construccion</h3>
+@endsection
+@section('content')
+<div class="col-md-12 col-sm-12 col-xs-12">
+	<div class="panel-content pagination2 table-responsive" >
+		  En Construccion
+  </div>
+</div> 
+@endsection
+@section('scripts')
+@endsection
